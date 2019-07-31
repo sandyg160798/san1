b3a4f2a29a19fc545dc11eec59c50b0d93fd5a3d
SANTHOSH KUMAR G <51796714+sandyg160798@users.noreply.github.com>
2019-07-31 15:49:28 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char b;
+    scanf("%s",&b);
+    if(b=='a'||b=='b'||b=='c'||b=='d'||b=='e'||b=='f'||b=='g'||b=='h'||b=='i'||b=='j'||b=='k'||b=='l'||b=='m'||b=='n'||b=='o'||b=='p'||b=='q'||b=='r'||b=='s'||b=='t'||b=='u'||b=='u'||b=='v'||b=='w'||b=='x'||b=='y'||b=='z')
+printf("Alphabet");
+else
+printf(" ");
+    return 0;
+}
