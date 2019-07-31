2a06594116f23bd58fb510850c98a739d413e8d7
SANTHOSH KUMAR G <51796714+sandyg160798@users.noreply.github.com>
2019-07-31 16:07:34 +0530

    Update s_3.c

--- s_3.c
+++ s_3.c
@@ -3,9 +3,9 @@ int main()
    int a;
    scanf("%d",&a);
    if(a%4==0&&a%100&&a%400)
-   printf("leap year");
+   printf("yes");
    else
-   printf("not an leap year");
+   printf("no");
    
 
     return 0;
